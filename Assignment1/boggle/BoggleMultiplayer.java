58fc4006f7cd399070a42af10f00e6dd36640db5
MoeezW <116523395+MoeezW@users.noreply.github.com>
2022-11-23 15:32:42 -0500

    Add files via upload

---
+++ Assignment1/boggle/BoggleMultiplayer.java
@@ -0,0 +1,10 @@
+package boggle;
+
+import java.util.*;
+
+/**
+ * The BoggleMultiplayer class that handles the game
+ * if 2 players want to play the game against each other.
+ */
+public class BoggleMultiplayer {
+}
