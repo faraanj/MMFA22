147fa43346e1da81fc1e10e52e1b8936e7352079
javaidfa <faraan.javaid@mail.utoronto.ca>
2022-11-30 13:31:14 -0500

    Fixing Model functionality and main usage with new classes

--- Assignment1/Main.java
+++ Assignment1/Main.java
@@ -1,18 +1,39 @@
+import boggle.BoggleController;
 import boggle.BoggleGame;
+import javafx.application.Application;
+import javafx.stage.Stage;
 
 /**
  * The Main class for the first Assignment in CSC207, Fall 2022
  */
-public class Main {
+public class Main extends Application {
     /**
-    * Main method. 
-    * @param args command line arguments.
-    **/
-    public static void main(String[] args) {
-        BoggleGame b = new BoggleGame();
-        b.giveInstructions();
-        b.playGame();
+     * Main method.
+     * @param args command line arguments.
 
+    public static void main(String[] args)  {
+    BoggleController b = new BoggleController();
+
+    //BoggleGame b = new BoggleGame();
+    //b.giveInstructions();
+    //b.playGame();
+    }
+     **/
+    /** Main method
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    /** Start the visualization
+     */
+    public void start(Stage primaryStage) throws Exception {
+        BoggleController b = new BoggleController();
+        b.start(primaryStage);
+
+        //BoggleGame b = new BoggleGame();
+        //b.giveInstructions();
+        //b.playGame();
     }
 
 }
--- Assignment1/boggle/BoggleModel.java
+++ Assignment1/boggle/BoggleModel.java
@@ -1,5 +1,4 @@
 package boggle;
-package boggle;
 
 import java.io.*;
 import java.util.*;
