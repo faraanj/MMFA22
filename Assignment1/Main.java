5c655516a3e1e682d84def6f9fc76901288cc5b1
MoeezW <moeez.waqas@mail.utoronto.ca>
2022-11-30 14:07:57 -0500

    DEV-1.3 Updated Main and Added a framework for BoggleMultiplayer from BoggleGame

--- Assignment1/Main.java
+++ Assignment1/Main.java
@@ -1,17 +1,34 @@
 import boggle.BoggleGame;
+import boggle.BoggleMultiplayer;
+
+import java.util.Objects;
+import java.util.Scanner;
 
 /**
- * The Main class for the first Assignment in CSC207, Fall 2022
+ * The Main class for the first the game boggle for the CSC207, Fall 2022 group project.
  */
 public class Main {
     /**
-    * Main method. 
-    * @param args command line arguments.
-    **/
+     * Main method.
+     * @param args command line arguments.
+     **/
     public static void main(String[] args) {
-        BoggleGame b = new BoggleGame();
-        b.giveInstructions();
-        b.playGame();
+        Scanner check = new Scanner(System.in);
+        System.out.println("Press 'M' for a multiplayer game or 'C' for a game against the computer.");
+        String letterCheck = check.nextLine();
+        while (!(Objects.equals(letterCheck.toLowerCase(), "m")) && !(Objects.equals(letterCheck.toLowerCase(), "c"))) {
+            System.out.println("Press 'M' for a multiplayer game or 'C' for a game against the computer.");
+            letterCheck = check.nextLine();
+        }
+        if (letterCheck.equalsIgnoreCase("c")) {
+            BoggleGame b = new BoggleGame();
+            b.giveInstructions();
+            b.playGame();
+        } else {
+            BoggleMultiplayer b = new BoggleMultiplayer();
+            b.giveInstructions();
+            b.playGame();
+        }
     }
 
 }
