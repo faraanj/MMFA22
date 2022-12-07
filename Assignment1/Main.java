1683b6b8af067ad49f54379fb54629ff36f3e4c3
muzammilashraf1 <muzammil.shanjer@mail.utoronto.ca>
2022-12-06 21:56:12 -0500

    [DEV-1.2] Commit to revert bad changes.
    Commit to revert bad changes for better merge.

--- Assignment1/Main.java
+++ Assignment1/Main.java
@@ -1,39 +1,17 @@
-import boggle.BoggleController;
 import boggle.BoggleGame;
-import javafx.application.Application;
-import javafx.stage.Stage;
 
 /**
  * The Main class for the first Assignment in CSC207, Fall 2022
  */
-public class Main extends Application {
+public class Main {
     /**
-     * Main method.
-     * @param args command line arguments.
-
-    public static void main(String[] args)  {
-    BoggleController b = new BoggleController();
-
-    //BoggleGame b = new BoggleGame();
-    //b.giveInstructions();
-    //b.playGame();
-    }
-     **/
-    /** Main method
-     */
+    * Main method. 
+    * @param args command line arguments.
+    **/
     public static void main(String[] args) {
-        launch(args);
-    }
-
-    /** Start the visualization
-     */
-    public void start(Stage primaryStage) throws Exception {
-        BoggleController b = new BoggleController();
-        b.start(primaryStage);
-
-        //BoggleGame b = new BoggleGame();
-        //b.giveInstructions();
-        //b.playGame();
+        BoggleGame b = new BoggleGame();
+        b.giveInstructions();
+        b.playGame();
     }
 
 }
