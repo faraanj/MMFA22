9dcc73b2310cdd85c5ce2c7a9027a3ecc164f824
Rehsa497 <116524647+Rehsa497@users.noreply.github.com>
2022-12-06 22:25:32 -0500

    [DEV 2.2] Completion of User Achievements

---
+++ Assignment1/boggle/LongestWord.java
@@ -0,0 +1,19 @@
+package boggle.achievements;
+
+
+import java.util.ArrayList;
+
+public class LongestWord implements Achievements{
+
+    @Override
+    public String getDescription(ArrayList<String> value) {
+        String longestWord = "";
+        for (int i = 0; i < value.size(); i++){
+            String curr_word = value.get(i);
+            if (curr_word.length() > longestWord.length()){
+                longestWord = curr_word;
+            }
+        }
+        return "Your longest word found is: " + longestWord;
+    }
+}
